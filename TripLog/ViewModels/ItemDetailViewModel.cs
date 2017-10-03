9d0feac63a382092ae44febc5ad6741977ae7e1b
Michael Prenez-Isbell <michael.isbell@gmail.com>
2017-10-02 22:03:38 -0400

    first commit

---
+++ TripLog/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TripLog
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ TripLog/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TripLog
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
